75dfceb647d146d7ff119529c1c6809712bdc573
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-03-28 00:25:38 +0530

    fc

--- src/DV/DV30ResizeArray.java
+++ src/DV/DV30ResizeArray.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class DV30ResizeArray {
 	
-	
+
+
 	public static void printArray(int[] arr) {
 		for(int i=0;i<arr.length;i++) {
 			System.out.print(arr[i]+" ");
